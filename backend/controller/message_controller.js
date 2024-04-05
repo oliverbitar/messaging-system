60b0643f5d363892e3dc1979c5689d692b209dc3
oliverbitar <oliverbitar@hotmail.com>
2024-04-04 23:51:01 -0400

    BE-2: Add socket

--- backend/controller/message_controller.js
+++ backend/controller/message_controller.js
@@ -1,9 +1,20 @@
 const environments = require('../env');
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(environments.database.connection, environments.database.settings);
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 exports.getChatsForUser = async (req, res, next) => {
     try {
+      io.on('receive', (socket) => {
+        io.on('send-message', message => {
+          io.broadcast.emit(message);
+        })
+      });
       const db = client.db('communication');
       const collection = db.collection('chat_messages');
       const examples = await collection.find({chat_id: parseInt(req.params.chatId)}).toArray();
